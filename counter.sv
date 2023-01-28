4e7aae05e6f83aa49598ff9d2efa31794055a8b6
Artur Smirnov <smirnov_as@proitr.ru>
2023-01-28 10:16:52 +0300

    Исправил баг

--- counter.sv
+++ counter.sv
@@ -8,8 +8,8 @@ always @(posedge clk) begin
 	if (key[0] == 1'b0)
 		counter <= 0;
 	else if (key[1] == 1'b1)
-		counter <= counter +1;
-	else counter <= counter - 1;
+		counter <= counter - 1;
+	else counter <= counter + 1;
 end
 
 assign led = counter [27:20];
