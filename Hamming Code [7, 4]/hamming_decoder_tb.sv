af39d4a56fc7af4884195d9f7207e724346146c1
Aoizora <aoizora@yandex.ru>
2023-03-25 23:43:38 +0300

    add testbench

---
+++ Hamming Code [7, 4]/hamming_decoder_tb.sv
@@ -0,0 +1,24 @@
+module hamming_decoder_tb();
+    logic clk;
+    logic [6:0] codeword;
+    logic [2:0] syndrome;
+    logic [3:0] data;
+    
+    hamming_decoder dut(clk, codeword, syndrome, data);
+    
+    initial begin
+        clk <= '0;
+        forever #10 clk <= ~clk;
+    end
+    
+    initial
+    begin
+        codeword = 7'b0011010;
+        #100
+        codeword = 7'b0101011;
+        #100
+        codeword = 7'b1010100;
+        #100
+        codeword = 7'b1111111;
+    end
+endmodule
